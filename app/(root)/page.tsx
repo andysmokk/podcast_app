ec5e6a1ed5c679f0408e67cf0ca0c6de17f65f1b
Andrii Konovalov <andy.smokk@gmail.com>
2024-06-11 20:54:19 +0200

    finished homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
 
 function Home() {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
+              key={id}
+              title={title}
+              description={description}
+              imgURL={imgURL}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
