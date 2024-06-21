a24afdee2530c1a7c147f9d5b6e25a0355a3ec65
Andrii Konovalov <andy.smokk@gmail.com>
2024-06-21 15:46:20 +0200

    finished CreatePodcast component

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useState } from "react";
+import { Loader } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
 
 // import { Button } from "@/components/ui/button";
 import {
@@ -25,23 +27,40 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import GeneratePodcast from "@/components/GeneratePodcast";
+import GenerateThumbnail from "@/components/GenerateThumbnail";
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
 });
 
 const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 
 const CreatePodcast = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [imagePrompt, setImagePrompt] = useState("");
+  const [imageStorageId, setImageStorageIdStorageId] =
+    useState<Id<"_storage"> | null>(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [audioUrl, setAudioUrl] = useState("");
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(
+    null
+  );
+  const [audioDuration, setAudioDuration] = useState(0);
+
   const [voiceType, setVoiceType] = useState<string | null>(null);
+  const [voicePrompt, setVoicePrompt] = useState("");
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      podcastTitle: "",
+      podcastDescription: "",
     },
   });
 
@@ -145,6 +164,29 @@ const CreatePodcast = () => {
                 </FormItem>
               )}
             />
+          </div>
+
+          <div className="flex flex-col pt-10">
+            <GeneratePodcast />
+
+            <GenerateThumbnail />
+
+            <div className="mt-10 w-full">
+              <Button
+                type="submit"
+                className="text-16 w-full bg-orange-1 py-4 font-extrabold
+                 text-white-1 transition-all duration-500 hover:bg-black-1"
+              >
+                {isSubmitting ? (
+                  <>
+                    <Loader size={20} className="animate-spin mr-2" />
+                    Submitting ...
+                  </>
+                ) : (
+                  "Submit & Publish Podcast"
+                )}
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
