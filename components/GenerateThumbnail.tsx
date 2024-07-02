3fef87222f10e78390ee4cce3c0dec3431742381
Andrii Konovalov <andy.smokk@gmail.com>
2024-07-02 21:38:15 +0200

    +

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react";
+
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 const GenerateThumbnail = () => {
-  return <div>GenerateThumbnail</div>;
+  const [isAiThumbnail, setIsAiThumbnail] = useState(false);
+
+  return (
+    <>
+      <div className="generate_thumbnail">
+        <Button
+          type="button"
+          onClick={() => setIsAiThumbnail(true)}
+          variant="plain"
+          className={cn("", { "bg-black-6": isAiThumbnail })}
+        >
+          Use AI to generate thumbnail
+        </Button>
+        <Button
+          type="button"
+          onClick={() => setIsAiThumbnail(false)}
+          variant="plain"
+          className={cn("", { "bg-black-6": !isAiThumbnail })}
+        >
+          Upload custom image
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default GenerateThumbnail;
