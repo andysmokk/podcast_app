3f8cd36ba19b7999af95dd46059e387228c89cc0
Andrii Konovalov <andy.smokk@gmail.com>
2024-07-18 22:53:05 +0200

    fixed Carousel component

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -37,11 +37,12 @@ const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
     <section className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">{index + 1}</div>
-            </div>
-          ))}
+          {slides &&
+            slides.map((index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__number">{index + 1}</div>
+              </div>
+            ))}
         </div>
       </div>
 
