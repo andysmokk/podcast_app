981e0ef1c33f04b7f49c196c81a3d7d22911162a
Andrii Konovalov <andy.smokk@gmail.com>
2024-07-26 21:48:18 +0200

    started to implement the PodcastPlayer component

---
+++ components/PodcastPlayer.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+const PodcastPlayer = () => {
+  return (
+    <div className={cn("sticky bottom-0 left-0 flex size-full flex-col")}>
+      PodcastPlayer
+    </div>
+  );
+};
+
+export default PodcastPlayer;
