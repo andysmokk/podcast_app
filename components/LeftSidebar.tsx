e130281588e00d2f0e488c3219bf76ccb107469b
Andrii Konovalov <andy.smokk@gmail.com>
2024-08-02 16:22:58 +0200

    finished MobileNav

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -23,7 +23,7 @@ const LeftSidebar = () => {
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
         >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden ml-2">
             Podcast App
           </h1>
         </Link>
