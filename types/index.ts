56fe0106ec881df319af48e86bb1b6eba1f4142b
Andrii Konovalov <andy.smokk@gmail.com>
2024-08-05 15:02:19 +0200

    added profile page and ProfileCard component

--- types/index.ts
+++ types/index.ts
@@ -76,3 +76,34 @@ export interface AudioContextType {
   audio: AudioProps | undefined;
   setAudio: React.Dispatch<React.SetStateAction<AudioProps | undefined>>;
 }
+
+export interface PodcastProps {
+  _id: Id<"podcasts">;
+  _creationTime: number;
+  audioStorageId: Id<"_storage"> | null;
+  user: Id<"users">;
+  podcastTitle: string;
+  podcastDescription: string;
+  audioUrl: string | null;
+  imageUrl: string | null;
+  imageStorageId: Id<"_storage"> | null;
+  author: string;
+  authorId: string;
+  authorImageUrl: string;
+  voicePrompt: string;
+  imagePrompt: string | null;
+  voiceType: string;
+  audioDuration: number;
+  views: number;
+}
+
+export interface ProfilePodcastProps {
+  podcasts: PodcastProps[];
+  listeners: number;
+}
+
+export interface ProfileCardProps {
+  podcastData: ProfilePodcastProps;
+  imageUrl: string;
+  userFirstName: string;
+}
